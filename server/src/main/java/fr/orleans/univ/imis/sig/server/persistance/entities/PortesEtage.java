ca4ff776d8b4e4ab069b9441f3389ae3268a1891
viren <vincent.renard@gmx.com>
2020-12-11 14:23:50 +0100

    dtos in - out

--- server/src/main/java/fr/orleans/univ/imis/sig/server/persistance/entities/PortesEtage.java
+++ server/src/main/java/fr/orleans/univ/imis/sig/server/persistance/entities/PortesEtage.java
@@ -1,16 +1,22 @@
 package fr.orleans.univ.imis.sig.server.persistance.entities;
 
+import lombok.AccessLevel;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
+@ToString
 @Table(name = "portes-etage")
 public class PortesEtage {
     @Id
-    private int id;
-    private int numero;
-    private int salle1;
-    private int salle2;
-    private int etage;
+    int id;
+    int numero;
+    int salle1;
+    int salle2;
+    int etage;
 }
